9d26531c6ab3836024a343cab1e6d30a3eaf1476
Marine Hakobyan <marihako@c2r12s4.42yerevan.am>
2023-02-19 14:34:09 +0400

    with bonus

--- ft_printf.c
+++ ft_printf.c
@@ -41,23 +41,6 @@ int	ft_put_hex(unsigned long long n, int type)
 	return (len);
 }
 
-// int	ft_putptr_fd(unsigned long long p)
-// {
-// 	int	len;
-// 	unsigned long long	number;
-
-// 	len = 0;
-// 	number = (unsigned long long)p;
-// 	// if (number < 0)
-// 	// {
-// 	// 	len += write(fd, "-", 1);
-// 	// 	number *= -1;
-// 	// }
-// 	len += ft_putstr_fd("0x", 1);
-// 	len += ft_putitoa_base_ptr((unsigned long long)p, "0123456789abcdef");
-// 	return (len);
-// }
-
 int	ft_ptrlen(uintptr_t ptr)
 {
 	int	len;
@@ -113,11 +96,41 @@ int base_choose(unsigned int nbr, char c)
 	return (ft_putitoa_base(nbr, "0123456789ABCDEF"));
 }
 
-int ft_type(va_list args, const char type)
+int ft_printf_bonus(const char type, void *arg)
+{
+ 	// || print[i] == '-' || print[i] == '#' || print[i] == '.' || print[i] == ' ' || print[i] == '0'
+	if (type == '+')
+	{
+		if (*((int*)(arg)) < 0)
+			return (write(1, "-",1));
+		else
+			return (write(1, "+",1));
+	}
+	else if(type == ' ')
+	{
+		if (*((int*)(arg)) >= 0)
+			return (write(1, " ",1));
+		else
+			return (0);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+int ft_type(va_list args, const char type, const char *print)
 {
 	int len;
+	int i;
 
+	i = 0;
 	len = 0;
+	if (print[i] == '+' || print[i] == '-' || print[i] == '#' || print[i] == '.' || print[i] == ' ' || print[i] == '0')
+	{
+		len += ft_printf_bonus(print[i], args);
+		i++;
+	}
 	if (type == 'c')
 		len += ft_putchar_fd((char)va_arg(args, int), 1);
 	else if (type == 's')
@@ -125,7 +138,7 @@ int ft_type(va_list args, const char type)
 	else if (type == 'd' || type == 'i')
 		len += ft_putnbr_fd(va_arg(args, int), 1);
 	else if (type == 'u')
-		len += ft_putnbr_fd(va_arg(args, unsigned int), 1); //long long?
+		len += ft_putnbr_fd(va_arg(args, unsigned int), 1);
 	else if (type == 'x' || type == 'X')
 		len += base_choose(va_arg(args, unsigned int), type);
 	else if (type == 'p')
@@ -142,18 +155,25 @@ int	ft_printf(const char *print, ...)
 	va_list	args;
 	int		i;
 	int		len;
-
-	va_start(args, print);
+	
 	i = 0;
 	len = 0;
+
+	va_start(args, print);
+
 	while(print[i])
 	{
+		
 		if(print[i] == '%')
-			len += ft_type(args, print[++i]);
+		{
+			i++;
+			len += ft_type(args, print[i], &print[i]);
+		}
 		else
 			len += ft_putchar_fd(print[i], 1);
-		++i;
+		i++;
 	}
 	va_end(args);
+
 	return (len);
 }
